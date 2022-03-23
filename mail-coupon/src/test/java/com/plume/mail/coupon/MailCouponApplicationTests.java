3a73d54ec38e5477adb8d600ed28925807fde48c
mochiyu-211 <mochiyu211@163.com>
2022-03-23 20:50:27 +0800

    3.23

---
+++ mail-coupon/src/test/java/com/plume/mail/coupon/MailCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plume.mail.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailCouponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ mail-ware/src/test/java/com/plume/mail/ware/MailWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plume.mail.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailWareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
