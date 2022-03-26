5b01e749ee6cb30003a345d5f61bf959dd67790b
mochiyu-211 <mochiyu211@163.com>
2022-03-26 17:20:02 +0800

    326

--- mail-coupon/src/main/java/com/plume/mail/coupon/MailCouponApplication.java
+++ mail-coupon/src/main/java/com/plume/mail/coupon/MailCouponApplication.java
@@ -1,5 +1,6 @@
 package com.plume.mail.coupon;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
