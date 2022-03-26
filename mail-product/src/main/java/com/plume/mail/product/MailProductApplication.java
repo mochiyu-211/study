05aa39411b498b36435455e6b93b59374e48a965
mochiyu-211 <mochiyu211@163.com>
2022-03-26 17:36:13 +0800

    326

--- mail-product/src/main/java/com/plume/mail/product/MailProductApplication.java
+++ mail-product/src/main/java/com/plume/mail/product/MailProductApplication.java
@@ -1,8 +1,10 @@
 package com.plume.mail.product;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com/plume/mail/product/dao")//mybatis-plus注解,扫描配置
 @SpringBootApplication
 public class MailProductApplication {
 
--- mail-product/src/test/java/com/plume/mail/product/MailProductApplicationTests.java
+++ mail-product/src/test/java/com/plume/mail/product/MailProductApplicationTests.java
@@ -1,13 +1,30 @@
 package com.plume.mail.product;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.service.additional.query.impl.QueryChainWrapper;
+import com.plume.mail.product.entity.BrandEntity;
+import com.plume.mail.product.service.BrandService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class MailProductApplicationTests {
 
+	@Autowired
+	BrandService brandService;
+
 	@Test
 	void contextLoads() {
+
+		/*BrandEntity brandEntity = new BrandEntity();
+		brandEntity.setBrandId(2L);
+		brandEntity.setName("apple");
+		brandService.save(brandEntity);*/
+		System.out.println(brandService.list(new QueryWrapper<BrandEntity>().eq("brand_id",1L)));
 	}
 
 }
