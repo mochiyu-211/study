57968cce6149cfb86d02cd5f18b2ace5c9bd8166
mochiyu-211 <mochiyu211@163.com>
2022-03-31 14:46:28 +0800

    331

--- mail-product/src/main/java/com/plume/mail/product/MailProductApplication.java
+++ mail-product/src/main/java/com/plume/mail/product/MailProductApplication.java
@@ -3,8 +3,10 @@ package com.plume.mail.product;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @MapperScan("com/plume/mail/product/dao")//mybatis-plus注解,扫描配置
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MailProductApplication {
 
